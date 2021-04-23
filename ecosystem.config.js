5d46dfa4b7eaf4bf10b3daf8754abc702854a6b1
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-04-23 14:15:38 +0700

    Update

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "discord-raisa",
+    script: "./index.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
